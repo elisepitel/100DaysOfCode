d0abadfbfa1ab548ce9ebeb323be8c38b1125474
Elise Pitel <[pitel.elise@gmail.com]>
2020-06-07 00:06:06 +0200

    new date format

--- daysOfCode.js
+++ daysOfCode.js
@@ -1,41 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => { 
-    
+document.addEventListener('DOMContentLoaded', () => {     
     var dateInput = document.getElementById('dateInput');
-    var futureDate = new Date();
+    var futureDateFormat = new Date();
     const calculate = document.getElementById('calculate');
 
+    // return date with correct ordinal
+    function dateOrdinal(d) {
+        if (d == 31 || d == 21 || d == 1) return d + "st";
+        else if (d == 22 || d == 2) return d + "nd";
+        else if (d == 23 || d == 3) return d + "rd";
+        else return d + "th";
+    };
+
+    // add x day(s) to the informed date
     function addDays(date, days) {
+        var futureDate = new Date();
+
         futureDate.setDate(futureDate.getDate() + days);
-        return futureDate;
+
+        //format the date 
+        var day = dateOrdinal(futureDate.getDay()); // day
+        var month = futureDate.getMonth(); // month (in integer 0-11)
+        var year = futureDate.getFullYear(); // year
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        futureDateFormat = day + " " + months[month] + " " + year;
+
+        return futureDateFormat;
     }
 
     calculate.addEventListener ('click', (e) => {
         if (dateInput.value){
             addDays(dateInput.value, 100);
-            alert("You are going to start the " + futureDate);
+            alert("You are going to end the 100 days of code challenge the " + futureDateFormat);
         } else {
             alert("You need to inform your starting date first")
         }
     });
 
 });
-
-
-
-
-    /*
-    window.onload = function(){
-    function getDate() {
-        var month_name = new Array('January', 'February', 'March',
-            'April', 'May', 'June', 'July', 'August', 'September',
-            'October', 'November', 'December');
-        var date = new Date();
-        var curr_month = date.getMonth();
-        var curr_year = date.getFullYear();
-        var input = document.getElementById('dateInput');
-        dateEntered.value = month_name[curr_month] + "," + curr_year;
-        console.log(input.value);
-    }
-    });
-  
-    }*/
