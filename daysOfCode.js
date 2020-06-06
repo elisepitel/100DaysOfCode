be1a90de8e0ed8051e3bcbcbbb7fad8680558fe7
Elise Pitel <[pitel.elise@gmail.com]>
2020-06-06 21:40:59 +0200

    update alert

--- daysOfCode.js
+++ daysOfCode.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => { 
+    
+    var dateInput = document.getElementById('dateInput').value;
+    //var dateEntered = new Date(dateInput);
 
-
- 
     const calculate = document.getElementById('calculate');
+
+
+    console.log(dateInput);
+
+
 
     calculate.addEventListener ('click', (e) => {
         alert("You'll finish the 100 days of code challenge the");
@@ -14,16 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-    /*function getDate() {
+    /*
+    window.onload = function(){
+    function getDate() {
         var month_name = new Array('January', 'February', 'March',
             'April', 'May', 'June', 'July', 'August', 'September',
             'October', 'November', 'December');
         var date = new Date();
         var curr_month = date.getMonth();
         var curr_year = date.getFullYear();
-        var input = document.getElementById('submitTask');
-        input.value = month_name[curr_month] + "," + curr_year;
+        var input = document.getElementById('dateInput');
+        dateEntered.value = month_name[curr_month] + "," + curr_year;
+        console.log(input.value);
     }
-    window.onload = function(){
-        getDate();
     }*/
